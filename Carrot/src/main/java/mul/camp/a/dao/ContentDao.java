33d79873d8505911bf84074f3b49492bf27264b9
Hertz <kljj0650@naver.com>
2022-01-12 14:00:48 +0900

    인터페이스 작성

--- Carrot/src/main/java/mul/camp/a/dao/ContentDao.java
+++ Carrot/src/main/java/mul/camp/a/dao/ContentDao.java
@@ -1,5 +1,21 @@
 package mul.camp.a.dao;
 
+import mul.camp.a.dto.ContentDto;
+
 public interface ContentDao {
     
+    // bid(게시판)에 해당하는 게시글 데이터 배열 반환
+    public ContentDto[] getList(int bid);
+
+    // cid(게시글)에 해당하는 게시글 데이터 반환
+    public ContentDto getContent(int cid);
+
+    // 입력받은 내용을 DB에 추가
+    public int addContent(ContentDto dto);
+
+    // 입력받은 내용으로 DB에 수정
+    public int updateContent(ContentDto dto);
+
+    // cid(게시글)에 해당하는 게시글을 DB에서 제거
+    public int deleteContent(int cid);
 }
