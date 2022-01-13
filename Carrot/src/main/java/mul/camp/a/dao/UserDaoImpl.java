4c2e5b17092cd40b6653abe098dcf8b00fd4f8c7
Hertz <kljj0650@naver.com>
2022-01-13 15:38:12 +0900

    UserDao 작성

--- Carrot/src/main/java/mul/camp/a/dao/UserDaoImpl.java
+++ Carrot/src/main/java/mul/camp/a/dao/UserDaoImpl.java
@@ -1,50 +1,60 @@
 package mul.camp.a.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import mul.camp.a.dto.UserDto;
 
 public class UserDaoImpl implements UserDao {
 
+    // 네임스페이스
     private final static String NAMESPACE = "User.";
     private final static String NAMESPACE_ADD_USER = NAMESPACE + "addUser";
     private final static String NAMESPACE_UPDATE_USER = NAMESPACE + "updateUser";
     private final static String NAMESPACE_DELETE_USER = NAMESPACE + "deleteUser";
     private final static String NAMESPACE_GET_USER = NAMESPACE + "getUser";
+    private final static String NAMESPACE_GET_USER_ID = NAMESPACE + "getUserId";
     private final static String NAMESPACE_GET_USER_LIST = NAMESPACE + "getUserList";
+
+    @Autowired
+    SqlSession session;
 
     @Override
     public int addUser(UserDto dto) {
-        // TODO Auto-generated method stub
-        return 0;
+        
+        return session.insert(NAMESPACE_ADD_USER, dto);
     }
 
     @Override
     public int updateUser(UserDto dto) {
-        // TODO Auto-generated method stub
-        return 0;
+        
+        return session.update(NAMESPACE_UPDATE_USER, dto);
     }
 
     @Override
     public int deleteUser(int uid) {
-        // TODO Auto-generated method stub
-        return 0;
+        
+        return session.delete(NAMESPACE_DELETE_USER, uid);
     }
 
     @Override
     public UserDto getUser(int uid) {
-        // TODO Auto-generated method stub
-        return null;
+        
+        return session.selectOne(NAMESPACE_GET_USER, uid);
     }
 
     @Override
     public UserDto getUser(String id) {
-        // TODO Auto-generated method stub
-        return null;
+        
+        return session.selectOne(NAMESPACE_GET_USER_ID, id);
     }
 
     @Override
-    public UserDto[] getUserList() {
-        // TODO Auto-generated method stub
-        return null;
+    public List<UserDto> getUserList() {
+        
+        return session.selectList(NAMESPACE_GET_USER_LIST);
     }
     
 }
