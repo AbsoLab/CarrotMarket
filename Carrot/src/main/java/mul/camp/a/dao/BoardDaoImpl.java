50231fc2ae8d870cccd69ce28ec8f45581f0afd9
Hertz <kljj0650@naver.com>
2022-01-13 15:41:16 +0900

    BoardDao 작성

---
+++ Carrot/src/main/java/mul/camp/a/dao/BoardDaoImpl.java
@@ -0,0 +1,23 @@
+package mul.camp.a.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BoardDaoImpl implements BoardDao {
+
+    public final static String NAMESPACE = "Board.";
+
+    public final static String NAMESPACE_GET_BOARD_LIST = NAMESPACE + "getBoardList";
+
+    @Autowired
+    SqlSession session;
+
+    @Override
+    public List<String> getBoardList() {
+        
+        return session.selectList(NAMESPACE_GET_BOARD_LIST);
+    }
+    
+}
