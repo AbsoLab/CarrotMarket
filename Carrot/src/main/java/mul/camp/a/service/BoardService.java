3cafd26d9209e3cd11f4f09fa9773cb7d4597cad
Hertz <kljj0650@naver.com>
2022-01-14 15:19:17 +0900

    반환값 변경, 함수 구조 수정, 답글 작성 함수 추가

--- Carrot/src/main/java/mul/camp/a/service/BoardService.java
+++ Carrot/src/main/java/mul/camp/a/service/BoardService.java
@@ -1,12 +1,17 @@
 package mul.camp.a.service;
 
+import java.util.List;
+
 import mul.camp.a.dto.ContentDto;
 import mul.camp.a.dto.ReplyDto;
 
 public interface BoardService {
 
+    // 게시판 이름 목록 반환
+    public List<String> boardList();
+
     // bid(게시판)에 해당하는 글 목록 반환
-    public ContentDto[] boardList(int bid);
+    public List<ContentDto> contentList(int bid);
 
     // cid(글)에 해당하는 글 데이터 반환
     public ContentDto content(int cid);
@@ -15,17 +20,20 @@ public interface BoardService {
     public boolean writeContent(ContentDto dto);
 
     // 글 수정
-    public boolean updateContent(int uid, ContentDto dto);
+    public boolean updateContent(ContentDto dto);
 
     // 글 삭제
-    public boolean deleteContent(int uid, int cid);
+    public boolean deleteContent(int cid);
 
     // 댓글 작성
     public boolean writeReply(ReplyDto dto);
 
+    // 답글 작성
+    public boolean writeReplyReply(ReplyDto dto);
+
     // 댓글 수정
-    public boolean updateReply(int uid, ReplyDto dto);
+    public boolean updateReply(ReplyDto dto);
 
     // 댓글 삭제
-    public boolean deleteReply(int uid, int rid);
+    public boolean deleteReply(int rid);
 }
