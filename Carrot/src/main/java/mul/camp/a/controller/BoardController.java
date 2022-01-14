fb294bfda523ace25d5a5159be2254c8e7c2eef5
Noh-jisu <nojisu123@naver.com>
2022-01-14 13:08:22 +0900

    임시

--- Carrot/src/main/java/mul/camp/a/controller/BoardController.java
+++ Carrot/src/main/java/mul/camp/a/controller/BoardController.java
@@ -1,8 +1,29 @@
 package mul.camp.a.controller;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import mul.camp.a.service.BoardService;
 
 @Controller
 public class BoardController {
+	
+	private static Logger logger = LoggerFactory.getLogger(BoardController.class);
+//	
+//	@Autowired
+//	BoardService service;
+//	
+	@RequestMapping(value = "sellBoard.do", method = RequestMethod.GET)
+	public String sellBoard() {
+		logger.info("BoardController sellBoard() " + new Date());
+		
+		return "sellBoard";
+	}
     
 }
---
+++ Carrot/src/main/webapp/WEB-INF/views/main.jsp
@@ -0,0 +1,22 @@
+<%@page import="mul.camp.a.dto.UserDto"%>
+<%@page import="java.util.List"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<%-- <%
+List<UserDto> userinfo = (List<UserDto>)request.getAttribute("login");
+%> --%>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1>메인페이지 입니다(index로 예상)</h1>
+<h2><a href="sellBoard.do">판매게시판으로 이동</a></h2>
+
+</body>
+</html>
