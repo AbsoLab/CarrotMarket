460490ad02045fc32c1e9b17b71abf77a768e2e5
HoQoo <an881201@naver.com>
2022-01-15 16:09:12 +0900

    UserServiceImpl 추가

---
+++ Carrot/src/main/java/mul/camp/a/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package mul.camp.a.service;
+
+import mul.camp.a.dto.UserDto;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public boolean login(UserDto dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean register(UserDto dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkIdDup(String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateUserInfo(UserDto dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public UserDto userInfo(int uid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteAccount(int uid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public UserDto[] userList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
